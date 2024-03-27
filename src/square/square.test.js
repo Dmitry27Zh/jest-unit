d7669b4b0a652e450e56daa2a8f70bf2a0faf3f2
Dmitry <dmitry27zh@gmail.com>
2024-03-27 18:07:32 +0500

    Use mocks

--- src/square/square.test.js
+++ src/square/square.test.js
@@ -12,6 +12,18 @@ describe('mapArrToStrings', () => {
     expect(square(2)).toBeGreaterThan(3)
     expect(square(2)).not.toBeUndefined()
   })
-  afterEach()
-  afterAll()
+  test('Math pow call', () => {
+    const spyMathPow = jest.spyOn(Math, 'pow')
+    square(2)
+    expect(spyMathPow).toBeCalledTimes(1)
+  })
+  test('No Math pow call', () => {
+    const spyMathPow = jest.spyOn(Math, 'pow')
+    square(1)
+    expect(spyMathPow).toBeCalledTimes(0)
+  })
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+  afterAll(() => {})
 })
