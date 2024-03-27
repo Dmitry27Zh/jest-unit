b1e8b6d9a6530164d148296605ec8813159cf676
Dmitry <dmitry27zh@gmail.com>
2024-03-27 17:40:35 +0500

    Use describe

--- src/validateValue/validateValue.test.js
+++ src/validateValue/validateValue.test.js
@@ -3,3 +3,15 @@ const validateValue = require('./validateValue')
 test('Value validation', () => {
   expect(validateValue(50)).toBe(true)
 })
+
+describe('validateValue', () => {
+  test('Correct value', () => {
+    expect(validateValue(50)).toBe(true)
+  })
+  test('Min', () => {
+    expect(validateValue(-1)).toBe(false)
+  })
+  test('Max', () => {
+    expect(validateValue(101)).toBe(false)
+  })
+})
