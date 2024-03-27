b3a4f526ab6aa5ecf2eac8e2de47410f5ae2a76a
Dmitry <dmitry27zh@gmail.com>
2024-03-27 17:55:52 +0500

    BeforeEach, beforeAll

--- src/square/square.test.js
+++ src/square/square.test.js
@@ -1,10 +1,17 @@
 const square = require('./square')
 
 describe('mapArrToStrings', () => {
+  let mockValue
+  beforeEach(() => {
+    mockValue = Math.random()
+  })
+  beforeAll(() => {})
   test('Correct value', () => {
     expect(square(2)).toBe(4)
     expect(square(2)).toBeLessThan(5)
     expect(square(2)).toBeGreaterThan(3)
     expect(square(2)).not.toBeUndefined()
   })
+  afterEach()
+  afterAll()
 })
